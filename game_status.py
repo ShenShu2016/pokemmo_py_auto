739af2ba83f0ea808a6434857d2136772512d577
Shen <shushen2013@gmail.com>
2023-05-15 01:41:19 -0400

    feat: all good

--- game_status.py
+++ game_status.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import copy
 import time
 from collections import deque
 from typing import TYPE_CHECKING
@@ -39,162 +40,145 @@ class GameStatus:
 
         # Calculate and return the ratio of black pixels
         black_ratio = black_pixels / total_pixels
+        self.game_status_dict["black_ratio"] = black_ratio
         return black_ratio
 
     def determine_game_status(self):
+        status_check_funcs = [
+            self.check_not_active_status,
+            self.check_normal_status,
+            self.check_battle_option_status,
+            self.check_battle_go_back_status,
+            self.check_battle_loading_status,
+        ]
+
+        for check_func in status_check_funcs:
+            status = check_func()
+            if status:
+                return status
+
+        return self.check_recent_status_or_return_unknown()
+
+    def check_not_active_status(self):
+        if self.game_status_dict.get("important") == "not active":
+            return self.set_and_return_status("Not Active Game Status")
+
+    def check_normal_status(self):
+        if self.game_status_dict.get("check_normal") > 60:
+            return self.set_and_return_status("Normal Game Status")
+
+    def check_battle_option_status(self):
+        if self.game_status_dict.get("battle_option_ORC") == True:
+            return self.set_and_return_status("Battle Option Status")
+
+    def check_battle_go_back_status(self):
+        if self.game_status_dict.get("battle_option_go_back_ORC") == True:
+            return self.set_and_return_status("Battle Go Back Status")
+
+    def check_battle_loading_status(self):
+        if self.game_status_dict.get("black_ratio") > 0.65:
+            return self.set_and_return_status("Battle loading Status")
+
+    def set_and_return_status(self, status):
+        self.game_status_dict["real_status"] = status
+        return status
+
+    def check_recent_status_or_return_unknown(self):
         timestamp = time.time()
+        self.game_status_dict["real_status"] = "Unknown Game Status"
 
-        # Check if the current status is "Not Active Game Status" or "Normal Game Status"
-        if (
-            "important" in self.game_status_dict
-            and self.game_status_dict["important"] == "not active"
-        ):
-            self.game_status_dict["real_status"] = "Not Active Game Status"
-            return "Not Active Game Status"
-        if (
-            "check_normal" in self.game_status_dict
-            and self.game_status_dict["check_normal"] > 60
-        ):
-            self.game_status_dict["real_status"] = "Normal Game Status"
-            return "Normal Game Status"
-
-        # Check if the current status is "Battle Game Status"
-        if (
-            "battle_option_ORC" in self.game_status_dict
-            and self.game_status_dict["battle_option_ORC"] == True
-        ):
-            self.game_status_dict["real_status"] = "Battle Option Status"
-            return "Battle Game Status"
-        elif (
-            "battle_option_go_back_ORC" in self.game_status_dict
-            and self.game_status_dict["battle_option_go_back_ORC"] == True
-        ):
-            self.game_status_dict["real_status"] = "Battle Go Back Status"
-            return "Battle Game Status"
-        elif self.game_status_dict["black_ratio"] > 0.65:
-            self.game_status_dict["real_status"] = "Battle loading Status"
-            return "Battle loading Status"
-        else:
-            # Look like it is an Unknown Game Status
-            self.game_status_dict["real_status"] = "Unknown Game Status"
-            # If no recent status change found, return the current status
-            if len(self.recent_status_game_status_dict_list) > 0:
-                for recent_timestamp, recent_status_dict in reversed(
-                    self.recent_status_game_status_dict_list
+        if self.recent_status_game_status_dict_list:
+            for recent_timestamp, recent_status_dict in reversed(
+                self.recent_status_game_status_dict_list
+            ):
+                recent_status = recent_status_dict.get("real_status")
+                if (
+                    recent_status != "Unknown Game Status"
+                    and timestamp - recent_timestamp <= 10
                 ):
-                    recent_status = recent_status_dict.get("real_status")
-                    if (
-                        recent_status != "Unknown Game Status"
-                        and timestamp - recent_timestamp <= 10
-                    ):
-                        return recent_status
+                    return recent_status
 
-        # If no recent status change found within 10 seconds, return "Unknown Game Status"
         return "Unknown Game Status"
+
+    def save_screenshot_check_status(self):
+        if time.time() - self.last_image_save_time >= 15:
+            self.last_image_save_time = time.time()
+            image = self.pokeMMO.get_most_recent_image_BRG()
+            gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            timestamp_str = time.strftime("%Y%m%d%H%M%S", time.localtime(time.time()))
+            filename = f"screenshot\image_{timestamp_str}.png"
+            cv2.imwrite(filename, gray_image)
+
+            if self.recent_images:
+                _, last_filename, _ = self.recent_images[-1]
+                last_image = cv2.imread(last_filename, cv2.IMREAD_GRAYSCALE)
+
+                result = cv2.matchTemplate(last_image, gray_image, cv2.TM_CCOEFF_NORMED)
+                min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+                self.game_status_dict["image_match_ratio"] = max_val
+                self.recent_images.append((time.time(), filename, max_val))
+
+                if all(
+                    image_data[2] is not None and image_data[2] >= 0.2
+                    for image_data in self.recent_images
+                ):
+                    self.game_status_dict["important"] = "not active"
+            else:
+                self.recent_images.append((time.time(), filename, None))
+
+    def check_normal(self):
+        my_name_ORC = self.pokeMMO.get_text_from_center(
+            box_width=125, box_height=25, offset_x=0, offset_y=104, config="--psm 7"
+        )
+        is_match, match_ratio = self.pokeMMO.word_recognizer.compare_with_target(
+            my_name_ORC,
+            target_words=target_words_dict["my_name_ORC"],
+            threshold=60,
+        )
+        self.game_status_dict["check_normal"] = match_ratio
+
+    def check_battle(self):
+        black_ratio = self.calculate_black_ratio()
+        self.game_status_dict["black_ratio"] = black_ratio
+        if black_ratio > 0.35:
+            self.check_battle_option()
+            self.check_battle_go_back()
+
+    def check_battle_option(self):
+        battle_option_x_y = (214, 482), (627, 583)
+        battle_option_ORC = self.pokeMMO.get_text_from_box_coordinate(
+            battle_option_x_y[0], battle_option_x_y[1]
+        )
+        is_match, match_ratio = self.pokeMMO.word_recognizer.compare_with_target(
+            battle_option_ORC, target_words_dict["battle_option_ORC"]
+        )
+        self.game_status_dict["battle_option_ORC"] = is_match
+
+    def check_battle_go_back(self):
+        battle_option_go_back_x_y = (1080, 558), (1142, 575)
+        battle_option_go_back_ORC = self.pokeMMO.get_text_from_box_coordinate(
+            battle_option_go_back_x_y[0], battle_option_go_back_x_y[1]
+        )
+        is_match, match_ratio = self.pokeMMO.word_recognizer.compare_with_target(
+            battle_option_go_back_ORC,
+            target_words_dict["battle_option_go_back_ORC"],
+        )
+        self.game_status_dict["battle_option_go_back_ORC"] = is_match
 
     def check_game_status(self):
         timestamp = time.time()
 
-        def save_screenshot_check_status(timestamp):
-            if time.time() - self.last_image_save_time >= 15:
-                self.last_image_save_time = time.time()
-                image = self.pokeMMO.get_most_recent_image_BRG()
-                timestamp_str = time.strftime("%Y%m%d%H%M%S", time.localtime(timestamp))
-                filename = f"screenshot\image_{timestamp_str}.png"
-                cv2.imwrite(filename, image)
-
-                # Compare with last image if available
-                if len(self.recent_images) > 0:
-                    _, last_filename, _ = self.recent_images[-1]
-                    last_image = cv2.imread(last_filename, cv2.IMREAD_GRAYSCALE)
-                    current_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                    result = cv2.matchTemplate(
-                        last_image, current_image, cv2.TM_CCOEFF_NORMED
-                    )
-                    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-                    self.game_status_dict["image_match_ratio"] = max_val
-
-                    # Save the image, timestamp and match ratio to recent_images
-                    self.recent_images.append((timestamp, filename, max_val))
-
-                    # Check if all max_vals in recent_images are not below 0
-                    if all(
-                        image_data[2] is not None
-                        and image_data[2] >= 0.2  #! 当所有匹配都大于0.2时，说明游戏状态没有变化
-                        for image_data in self.recent_images
-                    ):
-                        self.game_status_dict["important"] = "not active"
-                else:
-                    # For the first image, no comparison can be made, so match ratio is None
-                    self.recent_images.append((timestamp, filename, None))
-
-        def check_normal():  # use name to check status
-            my_name_ORC = self.pokeMMO.get_text_from_center(
-                box_width=125, box_height=25, offset_x=0, offset_y=104, config="--psm 7"
-            )  # Player Name and guild name
-            is_match, match_ratio = self.pokeMMO.word_recognizer.compare_with_target(
-                my_name_ORC,
-                target_words=target_words_dict["my_name_ORC"],
-                threshold=60,  #!
-            )
-            # print(
-            #     f"my_name_ORC: {my_name_ORC}, is_match: {is_match}, match_ratio: {match_ratio}"
-            # )
-            self.game_status_dict["check_normal"] = match_ratio  # 这里会导致我之前给的threshold失效
-            return is_match, match_ratio
-
-        def check_battle():
-            battle_status = None
-            battle_black_ratio = 0.35  # 有时候天气原因导致黑色比例下降
-            black_ratio = self.calculate_black_ratio()
-            self.game_status_dict["black_ratio"] = black_ratio
-            if black_ratio > battle_black_ratio:
-                battle_option_x_y = (214, 482), (627, 583)
-                battle_status = "battle_loading"
-                battle_option_ORC = self.pokeMMO.get_text_from_box_coordinate(
-                    battle_option_x_y[0], battle_option_x_y[1]
-                )
-                # print("battle_option_ORC", battle_option_ORC)
-                (
-                    is_match,
-                    match_ratio,
-                ) = self.pokeMMO.word_recognizer.compare_with_target(
-                    battle_option_ORC, target_words_dict["battle_option_ORC"]
-                )
-                # print("battle_option_ORC", is_match, match_ratio)
-                self.game_status_dict["battle_option_ORC"] = is_match
-
-                battle_option_go_back_x_y = (1080, 558), (1142, 575)
-                battle_option_go_back_ORC = self.pokeMMO.get_text_from_box_coordinate(
-                    battle_option_go_back_x_y[0], battle_option_go_back_x_y[1]
-                )
-                # print("get_text_from_box_coordinate", battle_option_go_back_ORC)
-                (
-                    is_match,
-                    match_ratio,
-                ) = self.pokeMMO.word_recognizer.compare_with_target(
-                    battle_option_go_back_ORC,
-                    target_words_dict["battle_option_go_back_ORC"],
-                )
-                self.game_status_dict["battle_option_go_back_ORC"] = is_match
-                if is_match:
-                    battle_status = (
-                        "battle_action_choice"  # 因为 go back 会出现在 fight bag pokemon 选项里面
-                    )
-
-            return battle_status
-
-        check_normal()
-        check_battle()
-        save_screenshot_check_status(timestamp)
-
-        print(self.game_status_dict)
+        self.check_normal()
+        self.check_battle()
+        self.save_screenshot_check_status()
 
         return_status = self.determine_game_status()
         self.game_status_dict["return_status"] = return_status
         self.recent_status_game_status_dict_list.append(
-            (timestamp, self.game_status_dict)
+            (timestamp, copy.deepcopy(self.game_status_dict))
         )
+        print(self.game_status_dict)
         return return_status
 
 
