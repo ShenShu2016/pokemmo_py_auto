2e92719f3a4e299e650d90726c08295fcb2e30b7
Shen <shushen2013@gmail.com>
2023-05-23 22:50:36 -0400

    feat: working

--- memory_injector.py
+++ memory_injector.py
@@ -7,15 +7,71 @@ from capstone import *
 from pymem.pattern import pattern_scan_all
 
 
+def format_address(address, length=8):
+    reversed_bytes = address.to_bytes(length, byteorder="little")
+    reversed_formatted = " ".join(f"{b:02X}" for b in reversed_bytes)
+    return reversed_formatted
+
+
+def to_signed_32_bit_le(i):
+    return struct.pack("<i", i)
+
+
+def calculate_jmp_offset(jmp_addr, target_addr):
+    offset = target_addr - (jmp_addr + 5)
+    return offset
+
+
+def get_lea_magic_number(aob_hex_list) -> str:
+    # Dictionary to map registers to hex numbers
+    register_to_hex = {
+        "r8": "40",
+        "r9": "41",
+        "r10": "42",
+        "r11": "43",
+        "r12": "44",
+        "r13": "45",
+        "r14": "46",
+        "r15": "47",
+    }
+
+    hex_str = "".join(aob_hex_list)
+    print(hex_str)
+    shellcode = bytes.fromhex(hex_str)
+    md = Cs(CS_ARCH_X86, CS_MODE_64)
+    for i in md.disasm(shellcode, 0x1000):
+        print("0x%x:\t%s\t%s" % (i.address, i.mnemonic, i.op_str))
+        op_str = i.op_str
+        matches = re.findall(r"r\d+", op_str)
+        if matches:
+            # Return the hex value corresponding to the register
+            register = matches[-1]  # assuming you want the first match
+            if register in register_to_hex:
+                return register_to_hex[register]
+        else:
+            raise Exception("No register found in operation")
+
+    raise Exception("No lea magic number found")
+
+
+def split_bytes_to_int(self, data, start, end):
+    converted = int.from_bytes(data[start:end], byteorder="little")
+    return converted
+
+
 class MemoryInjector:
     def __init__(self, target_process):
         self.target_process = target_process
         self.pm = pymem.Pymem(target_process)
         self.real_aob_offset = None
+        self.aob_address_list = []
         self.aob_hex_list = []
-        self.aob_address = self.aob_scan()  # 这个要放在aob_hex_list后面
+        self.aob_address_list = []
+        self.aob_address = 0  # 这个要放在aob_hex_list后面
         self.TR = pymem.memory.allocate_memory(self.pm.process_handle, 4)
         self.newmem = None
+
+        self.aob_address = self.aob_scan()
 
     def aob_scan(self):
         for i in range(10):
@@ -29,7 +85,7 @@ class MemoryInjector:
                 print(
                     f"{i} try, aob_address_list offset -1: {aob_address_list} Not unique"
                 )
-                aob_address = random.choice(aob_address_list) - 1
+                aob_address = aob_address_list[0] - 1
                 self.aob_hex_list = [
                     hex(b)[2:].zfill(2).upper()
                     for b in self.pm.read_bytes(aob_address, 5)
@@ -42,68 +98,11 @@ class MemoryInjector:
                 print(f"{i} try, retrying...")
         raise Exception("No aob found")
 
-    @staticmethod
-    def format_address(address, length=8):
-        reversed_bytes = address.to_bytes(length, byteorder="little")
-        reversed_formatted = " ".join(f"{b:02X}" for b in reversed_bytes)
-        return reversed_formatted
-
-    @staticmethod
-    def to_signed_32_bit_le(i):
-        return struct.pack("<i", i)
-
-    # Other methods omitted for brevity
-    @staticmethod
-    def calculate_jmp_offset(jmp_addr, target_addr):
-        offset = target_addr - (jmp_addr + 5)
-        return offset
-
-    def get_lea_magic_number(self) -> str:
-        # Dictionary to map registers to hex numbers
-        register_to_hex = {
-            "r8": "40",
-            "r9": "41",
-            "r10": "42",
-            "r11": "43",
-            "r12": "44",
-            "r13": "45",
-            "r14": "46",
-            "r15": "47",
-        }
-
-        hex_str = "".join(self.aob_hex_list)
-        print(hex_str)
-        # Convert the hex string to bytes
-        shellcode = bytes.fromhex(hex_str)
-        # The first argument is the architecture type, CS_ARCH_X86 for x86.
-        # The second argument is the mode, CS_MODE_64 for 64-bit mode.
-        md = Cs(CS_ARCH_X86, CS_MODE_64)
-
-        # Disassemble the code
-        for i in md.disasm(shellcode, 0x1000):
-            print("0x%x:\t%s\t%s" % (i.address, i.mnemonic, i.op_str))
-            op_str = i.op_str
-
-            # Find all occurrences of "r" followed by a number
-            matches = re.findall(r"r\d+", op_str)
-            if matches:
-                # Return the hex value corresponding to the register
-                register = matches[-1]  # assuming you want the first match
-                if register in register_to_hex:
-                    return register_to_hex[register]
-            else:
-                raise Exception("No register found in operation")
-
-        raise Exception("No lea magic number found")
-
     def inject_memory(self):
         # Existing code before this...
-        self.TR_address_reversed_formatted = self.format_address(self.TR)
-        # Add your specific offsets here
-        print("self.aob_hex_list:", self.aob_hex_list)
-        lea_magic_number = self.get_lea_magic_number()
+        self.TR_address_reversed_formatted = format_address(self.TR)
+        lea_magic_number = get_lea_magic_number(self.aob_hex_list)
         lea_eax_rx_x = ["41", "8D", lea_magic_number, "10"]
-        # movsx_esi_byte_ptr_rx_x = self.movsx_dict[self.real_aob_offset]
         push_rax = ["50"]
         move_TR_eax = ["A3"] + list(self.TR_address_reversed_formatted.split(" "))
         pop_rax = ["58"]
@@ -121,9 +120,9 @@ class MemoryInjector:
         # Calculate jmp offset 1 (newmem to aob_address)
         jmp_addr_1 = len(part_head_combine) + self.newmem
         target_addr_1 = self.aob_address + 5
-        offset_1 = self.calculate_jmp_offset(jmp_addr_1, target_addr_1)
+        offset_1 = calculate_jmp_offset(jmp_addr_1, target_addr_1)
         print("offset_1", offset_1)
-        offset_1_le = self.to_signed_32_bit_le(offset_1)
+        offset_1_le = to_signed_32_bit_le(offset_1)
         print("offset_1_le", offset_1_le)
 
         # Create shell code
@@ -143,8 +142,8 @@ class MemoryInjector:
         # Calculate jmp offset 2 (aob_address to newmem)
         jmp_addr_2 = self.aob_address
         target_addr_2 = self.newmem
-        offset_2 = self.calculate_jmp_offset(jmp_addr_2, target_addr_2)
-        offset_2_le = self.to_signed_32_bit_le(offset_2)
+        offset_2 = calculate_jmp_offset(jmp_addr_2, target_addr_2)
+        offset_2_le = to_signed_32_bit_le(offset_2)
 
         # Create JMP shell code to inject into aob_address
         inject_hex = jmp_place + [
@@ -171,25 +170,18 @@ class MemoryInjector:
                 byte_values.append(int.from_bytes(hv, "little"))
         return bytes(byte_values)
 
-    def print_and_convert_bytes(self, data, start, end):
-        converted = int.from_bytes(data[start:end], byteorder="little")
-        # print(converted)
-        return converted
-
     def read_data(self):
         data = self.pm.read_bytes(self.TR, 4)
-        print(data)
         value = int.from_bytes(data, byteorder="little")
-        print("value", value)
         x_address = value - 4 - 80
         print("x_address", x_address, hex(x_address))
         data = self.pm.read_bytes(x_address, 10 + 80)
         print("x_y_map_direction", data)
-        self.x_coords = self.print_and_convert_bytes(data, 0 + 80, 2 + 80)
-        self.y_coords = self.print_and_convert_bytes(data, 2 + 80, 4 + 80)
-        self.map_number = self.print_and_convert_bytes(data, 4 + 80, 5 + 80)
+        self.x_coords = split_bytes_to_int(data, 0 + 80, 2 + 80)
+        self.y_coords = split_bytes_to_int(data, 2 + 80, 4 + 80)
+        self.map_number = split_bytes_to_int(data, 4 + 80, 5 + 80)
         self.face_dir = hex(data[9 + 80])[-1]
-        self.transport = self.print_and_convert_bytes(data, 0, 2)
+        self.transport = split_bytes_to_int(data, 0, 2)
         data_dict = {
             "x_coords": self.x_coords,
             "y_coords": self.y_coords,
