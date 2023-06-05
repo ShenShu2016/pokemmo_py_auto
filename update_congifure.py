fa741ba0f2c094809af71ed0179d0ce685164bc9
Shen <shushen2013@gmail.com>
2023-06-05 18:32:58 -0400

    feat: delete useless files

--- update_congifure.py
+++ update_congifure.py
@@ -3,7 +3,6 @@ import os
 
 # Define the directory to scan and the configuration file
 template = "asserts/template"
-template_atlas = "asserts/template/atlas"
 config_file = "configure.json"
 coords_tracking = "asserts/coords_tracking"
 
@@ -24,16 +23,6 @@ for file in files:
         key = os.path.splitext(file)[0]
         # Use the full path to the file as the value
         value = os.path.join(template, file)
-        new_config[key + "_path"] = value
-
-atlas_files = os.listdir(template_atlas)
-for file in atlas_files:
-    # Only consider .png files
-    if file.endswith(".png"):
-        # Use the name of the file (without the extension) as the key
-        key = os.path.splitext(file)[0]
-        # Use the full path to the file as the value
-        value = os.path.join(template_atlas, file)
         new_config[key + "_path"] = value
 
 coords_tracking_files = os.listdir(coords_tracking)
